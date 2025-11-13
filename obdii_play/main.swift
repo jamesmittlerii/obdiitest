94ba4ddd51bd47ac808f1a3987b670831602bf79
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-11-13 12:12:06 -0800

    fix state leak in unit conversion

--- obdii_play/main.swift
+++ obdii_play/main.swift
@@ -94,7 +94,9 @@ Task {
         let response = try await obdService.requestPID(.mode1(.status), unit: MeasurementUnit.metric)
         print(response)
         
+        print(try await obdService.requestPID(.mode1(.maf), unit: MeasurementUnit.metric))
         print(try await obdService.requestPID(.mode1(.maf), unit: MeasurementUnit.imperial))
+        print(try await obdService.requestPID(.mode1(.maf), unit: MeasurementUnit.metric))
       
         
         //print(try await obdService.requestPID(.mode1(.O2Sensor7WRCurrent), unit: MeasurementUnit.metric))
